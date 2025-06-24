3f5fa130524e8bdcb9defa1c9753c2027ecdf115
Martin Wael <martinwael91@gmail.com>
2025-06-24 11:46:57 +0300

    definition of function added

---
+++ text_utility.cpp
@@ -0,0 +1,45 @@
+#include "text_utility.h"
+
+int wordCount(char* str)
+{
+    return ;
+}
+
+int charCount(char* str)
+{
+
+    return; 
+}
+
+void vowelConsonantCount(char* str, int &vowels, int &consonants)
+{
+
+
+}
+
+void reverseString(char* str)
+{
+
+
+}
+
+bool isPalindrome(char* str)
+{
+
+
+}
+void toUpperCase(char* str)
+{
+
+
+}
+void toLowerCase(char* str)
+{
+
+}
+
+int counterSubstringOccurrence(char* str, char* substr)
+{
+
+    return ; 
+}
